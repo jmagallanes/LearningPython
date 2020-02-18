80f5d840d967cfe3ba247d70cd18c85fed767357
Jose Magallanes <jmmagal1@uci.edu>
2020-02-17 21:56:42 -0800

    Solved string1 A

--- Google Excercises/basic/string1.py
+++ Google Excercises/basic/string1.py
@@ -24,7 +24,10 @@
 # So donuts(5) returns 'Number of donuts: 5'
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
-  # +++your code here+++
+  if count >= 10:
+    return 'Number of donuts: many'
+  else:
+    return 'Number of donuts: ' + str(count)
   return
 
 
